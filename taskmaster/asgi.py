9c74f14ed2c7fd595e132277e44c909bef4a1aa9
Louis Zirkel III <zzzirk@gmail.com>
2023-10-19 15:39:44 -0600

    Initial commit

---
+++ taskmaster/asgi.py
@@ -0,0 +1,19 @@
+"""
+ASGI config for taskmaster project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+# Python imports
+import os
+
+# PIP imports
+from django.core.asgi import get_asgi_application
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taskmaster.settings")
+
+application = get_asgi_application()
